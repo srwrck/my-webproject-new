a841d579380c6b5fe83beccbc3c274f727d23590
Meow <Saintmaria2383@gmail.com>
2025-08-20 11:26:15 +0700

    creat bgColor and navbar

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -1,24 +1,22 @@
-// filepath: d:\tsxProjext\my-webproject\src\components\layout\Navbar\Navbar.tsx
 import Link from "next/link";
-import style from "./Navbar.module.css";
 
 export default function Navbar() {
   return (
-    <nav className={style.containerNav}>
-      <h1 className={style.title}>My Portfolio</h1>
-      <ul className={style.navLinks}>
+    <nav className="flex justify-between items-center p-4 bg-white outline-groove">
+      <h1 className="text-2xl font-bold">My Portfolio</h1>
+      <ul className="flex gap-4 list-none ">
         <li>
-          <Link href="/" className={style.navLink}>
+          <Link href="/" className="text-gray-700 hover:text-blue-500">
             Home
           </Link>
         </li>
         <li>
-          <Link href="/about" className={style.navLink}>
+          <Link href="/about" className="text-gray-700 hover:text-blue-500">
             About
           </Link>
         </li>
         <li>
-          <Link href="/contact" className={style.navLink}>
+          <Link href="/contact" className="text-gray-700 hover:text-blue-500">
             Contact
           </Link>
         </li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/app/**/*.{js,ts,jsx,tsx,css}",
-    "./src/components/**/*.{js,ts,jsx,tsx,css}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
